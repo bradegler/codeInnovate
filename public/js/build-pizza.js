df992eeb66d9201fc22b79c9a50dffa22399ed1d
Lovina Moses Dmello <lovinadmello@Lovinas-MacBook-Pro.local>
2019-02-21 15:47:29 -0500

    updated ingredients list

--- public/js/build-pizza.js
+++ public/js/build-pizza.js
@@ -1,20 +1,55 @@
-var cardHead = '<div class="col-md-3 mb-5"><div class="card-body"><h2 class="card-title"></h2><p class="card-text">';
-var cardImg = '<img class="img-fluid rounded mb-4 mb-lg-0" src="$$IMAGE$$" alt="" /> </p> </div>';
-var cardContent = '<div class="card-title text-center"><p>$$NAME$$</p></div></div>';
+var card = `
+<div class="card col-md-4 mb-5">
+    <div class="card-body">
+      <h2 class="card-title"></h2>
+      <p class="card-text">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="$$IMAGE$$" alt=""/> 
+      </p> 
+    </div>
+    <div class="card-title text-center">
+      <p>$$NAME$$</p>
+    </div>
+  <div class="card-footer ">
+      <div class="row buttonRow">
+        <a href="#" class="btn btn1 text-white col-md-2 btn2 active" id ="None">None</a>
+        <a href="#" class="btn btn1 text-white col-md-2 btn2 active" id="Light">Light</a>
+        <a href="#" class="btn btn1 text-white col-md-3 btn2 active" id="Normal">Normal</a>
+        <a href="#" class="btn btn1 text-white col-md-2 btn2 active" id="Extra">Extra</a>
+      </div>
+      
+  </div >
+</div >`;
 
-$(document).ready(function () {
+$(document).ready(function() {
   console.log("on load");
 
   $.ajax({
-    url: "http://129.213.143.54:30000/api/database/ingredients",
-  }).done(function (result) {
+    url: "http://129.213.143.54:30000/api/database/ingredients"
+  }).done(function(result) {
     console.log(result);
     var vegRow = $("#veggie-row");
+    var meatRow = $("#meat-row");
+    var cheeseRow = $("#cheese-row");
     result.forEach(element => {
       if (element.INGRE_TYPE_CODE === "MMVG") {
-        vegRow.append(cardHead + cardImg.replace("$$IMAGE$$", element.INGRE_IMG) + cardContent.replace("$$NAME$$", element.INGRE_NAME));
+        vegRow.append(
+          card
+            .replace("$$IMAGE$$", element.INGRE_IMG)
+            .replace("$$NAME$$", element.INGRE_NAME)
+        );
+      } else if (element.INGRE_TYPE_CODE === "MMM") {
+        meatRow.append(
+          card
+            .replace("$$IMAGE$$", element.INGRE_IMG)
+            .replace("$$NAME$$", element.INGRE_NAME)
+        );
+      } else if (element.INGRE_TYPE_CODE === "MMCS") {
+        cheeseRow.append(
+          card
+            .replace("$$IMAGE$$", element.INGRE_IMG)
+            .replace("$$NAME$$", element.INGRE_NAME)
+        );
       }
     });
   });
-
 });
